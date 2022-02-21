8068d65ffffc0056c7124352febacad7e2111269
Isidoro Legido Martínez <islegmar@gmail.com>
2022-02-21 11:55:05 +0100

    Cleanup + when clicking in a Task in the Gantt a detail FTEs for that task is shown. Also now all the tasks appear in the Gantt, it does not matter if they have start / end date.

--- modules/app/planning.js
+++ modules/app/planning.js
@@ -8,51 +8,12 @@ import * as TS  from '/modules/app/timeseries.js';
 document.addEventListener("custom.planning.refresh", function (evt) {
   const eContainer=evt.detail.container;
   buildGantt(eContainer.querySelector('#gantt'), evt.detail.jstree);
-  buildPlanning(eContainer, evt.detail.jstree);
 });
-
-/**
- * Used to group the dates (by month-year)
- */ 
-function getKeyDate(dt) {
-  return (dt.getMonth() + 1).toString().padStart(2, "0") + "-" + dt.getFullYear().toString().substring(2);
-}
-
-/* Given the denomination of a group in format mm-yy (see getKeyDate) , it gives the start and end date. */
-function getDatesInGroup(group) {
-  var start=null;
-  var end=null;
-  const values=group.match(/(\d+)-(\d+)/);
-  if ( values ) {
-    const month=parseInt(values[1]-1);
-    const year=values[2].length==2 ? 2000 + parseInt(values[2]) : parseInt(values[2]);
-    Log.log_is_low_debug() && Log.log_low_debug("group : '" + group + "' => month (start 0) : '" + month + "', year : '" + year + "'");
-    start=new Date(year, month, 1);
-    end=new Date(year, month, 1);
-
-    while ( true ) {
-      end.setDate(end.getDate()+1);
-      if ( end.getMonth()!=month ) {
-        end.setDate(end.getDate()-1);
-        break;
-      }
-    }
-
-  } else {
-    throw new Eror("Group '" + group + "' does not have the format month-year");
-  }
-
-  Log.log_is_low_debug() && Log.log_low_debug("group : '" + group + "' => start : '" + start + "', end : '" + end + "'");
-
-  return {
-    'start' : start,
-    'end' : end
-  };
-}
 
 // ----------------------------------------------------------------------- Gantt
 /**
- * Build the table with the FTEs and also add the listerner to show the details.
+ * Build the table with the Gantt that contains all the tasks / periods.
+ * If a Task is clicable, a detail view for the FTEs for that task is shown
  */
 function buildGantt(eTable, jstree) {
   removeChildren(eTable);
@@ -72,6 +33,10 @@ function buildGantt(eTable, jstree) {
 
     var eCell=document.createElement('div');
     eCell.innerHTML=item['name'];
+    eCell.classList.add("busy");
+    eCell.classList.add("level_" + item.level);
+    eCell.classList.add("clicable");
+    eCell.addEventListener('click', buildPlanning.bind(this, document.querySelector("#detail_task_by_rol"), jstree, item.node), false);
     eRow.appendChild(eCell);
 
     gantt.headers.forEach(name => {
@@ -84,7 +49,7 @@ function buildGantt(eTable, jstree) {
         var eSpan=document.createElement('span');
         eCell.appendChild(eSpan);
 
-        if ( item.periods[period_name] ) {
+        if ( item.periods && item.periods[period_name] ) {
           eSpan.classList.add("busy");
           eSpan.classList.add("level_" + item.level);
 
@@ -138,6 +103,11 @@ function getGantt(jstree) {
   var list=[];
 
   walk_tree(jstree, (node, level) => {
+    var info={
+      'name'  : node.text,
+      'level' : level,
+      'node'  : node
+    };
     if ( node.data.start_date && node.data.end_date ) {
       const my_start=DateUtils.str2Date(node.data.start_date);
       const my_end=DateUtils.str2Date(node.data.end_date);
@@ -145,12 +115,9 @@ function getGantt(jstree) {
       if ( !start_date || my_start<start_date) start_date=my_start;
       if ( !end_date || my_end>end_date) end_date=my_end;
 
-      list.push({
-        'name'  : node.text,
-        'level' : level,
-        'periods' : groupListElements(DateUtils.getListDates(my_start, my_end), getKeyDate)
-      });
+      info['periods'] = groupListElements(DateUtils.getListDates(my_start, my_end), getKeyDate)
     }
+    list.push(info);
   });
 
   const period_names=getGroupListElements(DateUtils.getListDates(start_date, end_date), getKeyDate);
@@ -163,12 +130,17 @@ function getGantt(jstree) {
 }
 
 // -------------------------------------------------------------------- Planning
-function buildPlanning(eContainer, jstree) {
-  var eTable=eContainer.querySelector('#planning');
+// TODO: change the name, this is confusing.
+// Here we show, given a Task, the detail FTEs for the different periods
+function buildPlanning(eContainer, jstree, node) {
+  eContainer.querySelector('.header').innerHTML = node.text;
+
+  var eTable=eContainer.querySelector('.content');
 
   removeChildren(eTable);
-  const planning=getPlanning(jstree);
+  const planning=getPlanning(jstree, node);
 
+  // Header: Rol + Periods
   var eHeader=document.createElement('div');
   eTable.appendChild(eHeader);
   planning.headers.forEach(name => {
@@ -176,9 +148,6 @@ function buildPlanning(eContainer, jstree) {
     eCell.innerHTML=name;
     eCell.classList.add('clicable');
     eHeader.appendChild(eCell);
-    if ( name !== "Task" ) {
-      eCell.addEventListener('click', showDetailByPeriod.bind(this, eContainer.querySelector("#detail_by_period"), name, jstree), false);
-    }
   });
 
   planning.list.forEach(item => {
@@ -189,8 +158,6 @@ function buildPlanning(eContainer, jstree) {
     eCell.innerHTML=item['Rol'];
     eCell.classList.add('clicable');
     eRow.appendChild(eCell);
-
-    eCell.addEventListener('click', showDetailByRol.bind(this, eContainer.querySelector("#detail_by_rol"), item['Rol'], jstree), false);
 
     planning.headers.forEach(name => {
       if ( name !== 'Rol' ) {
@@ -210,14 +177,14 @@ function buildPlanning(eContainer, jstree) {
  * - Header : periods of fime
  * - Row    : every row is a rol and for every period show the infor about FTEs.
  */
-export function getPlanning(jstree) {
+export function getPlanning(jstree, root) {
   // 1> For the root node (project)
   // - Get all the Simple Tasks
   // - For every day, sum the FTEs for every rol for all the tasks
   // - Maks monthly groups with all those FTEs.
   // - Calculate dtatistichal data as max/min/avg
   var all_ts={};
-  get_simple_nodes(jstree, null).forEach(node => {
+  get_simple_nodes(jstree, root).forEach(node => {
     TS.extendsTS(all_ts, TS.getNodeTS(node));
   });
   TS.collapseTSPoints(all_ts);
@@ -270,196 +237,17 @@ export function getPlanning(jstree) {
   }
 }
 
+// ---------------------------------------------------------- Internal Functions
+
 /**
- * Show the detail of the FTEs for a certain rol.
+ * Given a task (node), show the FTE's info for the different roles.
  */
-function showDetailByRol(eContainer, rol, jstree) {
+function periodshowDetailTaskByRol(eContainer, jstree, node) {
   eContainer.querySelector(".header").innerHTML=rol;
 
   var eTable=eContainer.querySelector(".content");
   removeChildren(eTable);
 
-  // 1> Get the data
-  var list=[];
-  var start_date=null;
-  var end_date=null;
-  // Here every row is a task (composed/simple) as in gantt but 
-  // where we're going to show the info in the simple tasks for 
-  // participation of that rol in the different periods
-  walk_tree(jstree, (node, level) => {
-    if ( node.data.start_date && node.data.end_date ) {
-      const my_start=DateUtils.str2Date(node.data.start_date);
-      const my_end=DateUtils.str2Date(node.data.end_date);
-
-      if ( !start_date || my_start<start_date) start_date=my_start;
-      if ( !end_date || my_end>end_date) end_date=my_end;
-
-      var item={
-        'name'  : node.text,
-        'level' : level,
-        'periods' : groupListElements(DateUtils.getListDates(my_start, my_end), getKeyDate),
-        'info' : null
-      };
-
-      if ( !node.data.isComposed && node.data.md[rol] ) { 
-        item.info={};
-        var total_days=0;
-        for(const period in item.periods ) {
-          var period_days=0;
-          item.periods[period].forEach( dt => {
-            if ( !DateUtils.isWeekend(dt) ) ++period_days;
-          });
-          total_days += period_days;
-          item.info[period] = {
-            'period_days' : period_days
-          }
-        }
-        var ftes = node.data.md[rol]/total_days;
-        for(const period in item.info ) {
-          item.info[period]["ftes"]=ftes;
-          item.info[period]["task"]=node.data.md[rol];
-          item.info[period]["rol_days"]=ftes*item.info[period]["period_days"];
-        }
-      }
-
-      list.push(item);
-    }
-  });
-
-  // 2> Build the table
-  // - Header : periods
-  // - Every row : task (composed/simple) marking the periods and info when the rol participates
-  const period_names=Object.keys(groupListElements(DateUtils.getListDates(start_date, end_date), getKeyDate)).sort();
-  var headers=['Task', ...period_names];
-
-  var eHeader=document.createElement('div');
-  eTable.appendChild(eHeader);
-  headers.forEach(name => {
-    var eCell=document.createElement('div');
-    eCell.innerHTML=name;
-    eHeader.appendChild(eCell);
-  });
-
-  list.forEach(item => {
-    var eRow=document.createElement('div');
-    eTable.appendChild(eRow);
-
-    var eCell=document.createElement('div');
-    eCell.innerHTML=item['name'];
-    eRow.appendChild(eCell);
-
-    headers.forEach(name => {
-      if ( name !== 'Task' ) {
-        var eCell=document.createElement('div');
-        eRow.appendChild(eCell);
-
-        if ( item.info && item.info[name]) {
-          const my_info = item.info[name];
-
-          eCell.classList.add("working");
-          eCell.innerHTML  = "";
-          eCell.innerHTML += "<b>Days : "     + formatString(my_info.period_days) + "</b><br/>";
-          eCell.innerHTML += "<b>Rol Days : " + formatString(my_info.rol_days)    + "</b><br/>";
-          eCell.innerHTML += "Task : "     + formatString(my_info.task) + "<br/>";
-          eCell.innerHTML += "FTEs : "     + formatString(my_info.ftes);
-        } else {
-          eCell.classList.add("busy");
-          eCell.classList.add("level_" + item.level);
-        }
-      }
-    });
-  });
-}
-
-/**
- * Show the detail of the FTEs for a certain Period
- * - Header : the roles.
- * - Every row is a task with info if that rol is involved
- */
-function showDetailByPeriod(eContainer, period, jstree) {
-  eContainer.querySelector(".header").innerHTML=period;
-
-  var eTable=eContainer.querySelector(".content");
-  removeChildren(eTable);
-
-  // 1> Get the data
-  var roles=[];
-  var list=[];
-  // Here every row is a task (composed/simple) as in gantt but 
-  // where we're going to show the info in the simple tasks for 
-  // participation of the different roles for thar period
-  walk_tree(jstree, (node, level) => {
-    var item={
-      'name'  : node.text,
-      'level' : level
-    };
-    if ( node.data.start_date && node.data.end_date ) {
-      const my_start=DateUtils.str2Date(node.data.start_date);
-      const my_end=DateUtils.str2Date(node.data.end_date);
-      const list_dates=DateUtils.getListDates(my_start, my_end);
-      const total_working_days=DateUtils.getTotWorkingDays(list_dates);
-
-      const groups=groupListElements(list_dates, getKeyDate);
-      if ( groups[period] ) {
-        const period_working_days=DateUtils.getTotWorkingDays(groups[period]);
-        for(const rol in node.data.md ) {
-          if ( !roles.includes(rol) ) roles.push(rol);
-
-          var ftes = node.data.md[rol]/total_working_days;
-          item[rol]={
-            "period_days" : period_working_days,
-            "ftes"        : ftes,
-            "task"        : node.data.md[rol],
-            "rol_days"    : ftes*period_working_days
-          };
-        }
-      }
-    }
-    list.push(item);
-  });
-
-  // 2> Build the table
-  // - Header : roles
-  // - Every row : task (composed/simple) marking the periods and info when the rol participates
-  var headers=['Task', ...roles];
-
-  var eHeader=document.createElement('div');
-  eTable.appendChild(eHeader);
-  headers.forEach(name => {
-    var eCell=document.createElement('div');
-    eCell.innerHTML=name;
-    eHeader.appendChild(eCell);
-  });
-
-  list.forEach(item => {
-    var eRow=document.createElement('div');
-    eTable.appendChild(eRow);
-
-    var eCell=document.createElement('div');
-    eCell.innerHTML=item['name'];
-    eRow.appendChild(eCell);
-
-    headers.forEach(name => {
-      if ( name !== 'Task' ) {
-        var eCell=document.createElement('div');
-        eRow.appendChild(eCell);
-
-        if ( item[name] ) {
-          const my_info = item[name];
-
-          eCell.classList.add("working");
-          eCell.innerHTML  = "";
-          eCell.innerHTML += "<b>Task : "     + formatString(my_info.task)     + "</b><br/>";
-          eCell.innerHTML += "<b>FTEs : "     + formatString(my_info.ftes)     + "</b><br/>";
-          eCell.innerHTML += "<b>Rol Days : " + formatString(my_info.rol_days) + "</b><br/>";
-          eCell.innerHTML += "Days : "     + formatString(my_info.period_days);
-        } else {
-          eCell.classList.add("busy");
-          eCell.classList.add("level_" + item.level);
-        }
-      }
-    });
-  });
 }
 
 /**
@@ -473,4 +261,43 @@ function get_simple_nodes(jstree, root) {
   }, root);
 
   return list;
+}
+
+/**
+ * Used to group the dates (by month-year)
+ */ 
+function getKeyDate(dt) {
+  return (dt.getMonth() + 1).toString().padStart(2, "0") + "-" + dt.getFullYear().toString().substring(2);
+}
+
+/* Given the denomination of a group in format mm-yy (see getKeyDate) , it gives the start and end date. */
+function getDatesInGroup(group) {
+  var start=null;
+  var end=null;
+  const values=group.match(/(\d+)-(\d+)/);
+  if ( values ) {
+    const month=parseInt(values[1]-1);
+    const year=values[2].length==2 ? 2000 + parseInt(values[2]) : parseInt(values[2]);
+    Log.log_is_low_debug() && Log.log_low_debug("group : '" + group + "' => month (start 0) : '" + month + "', year : '" + year + "'");
+    start=new Date(year, month, 1);
+    end=new Date(year, month, 1);
+
+    while ( true ) {
+      end.setDate(end.getDate()+1);
+      if ( end.getMonth()!=month ) {
+        end.setDate(end.getDate()-1);
+        break;
+      }
+    }
+
+  } else {
+    throw new Eror("Group '" + group + "' does not have the format month-year");
+  }
+
+  Log.log_is_low_debug() && Log.log_low_debug("group : '" + group + "' => start : '" + start + "', end : '" + end + "'");
+
+  return {
+    'start' : start,
+    'end' : end
+  };
 }
